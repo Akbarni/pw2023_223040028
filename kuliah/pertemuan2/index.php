510dea2bc7770991207bfb7f2cd026bec1251491
Akbarni <rediakbar082@gmail.com>
2023-02-20 21:19:08 +0700

    tugas

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,16 @@
+<?php $nama = "akbar"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 2</title>
+</head>
+<body>
+    <h1>
+        <?php echo "hello, $nama"; ?>
+    </h1>
+    <p><?php echo "pemograman web" ?></p>
+</body>
+</html>
